f67972c5e80ab43f8ab84ef64334a1f70d4e91d1
Uthaeus <romanlavery@gmail.com>
2023-04-10 20:45:53 -0600

    created migration to add phone number to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_11_022727) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_11_024214) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_11_022727) do
     t.string "roles"
     t.string "last_name"
     t.string "first_name"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
