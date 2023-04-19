c7490cd601bb80a938eed3e2373bae8de8baa41b
Uthaeus <romanlavery@gmail.com>
2023-04-19 13:14:40 -0600

    added permit parameters for devise whitelist

--- app/controllers/concerns/devise_whitelist.rb
+++ app/controllers/concerns/devise_whitelist.rb
@@ -6,8 +6,8 @@ module DeviseWhitelist
     end
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :first_name, :last_name, :password, :password_confirmation, :phone_number])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:email, :first_name, :last_name, :password, :password_confirmation, :phone_number])
     end
 
 end
